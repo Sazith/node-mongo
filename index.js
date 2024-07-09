74ca8891a86c8c392f74731f3fc4b2a01ee25073
Sazith <sazith.simecsoftdev@gmail.com>
2024-07-09 18:38:57 +0600

    add dyanmic url

--- index.js
+++ index.js
@@ -2,8 +2,33 @@
 
  const app = express();
 
+ const users = ["Asad", "Moin", "Sabed", "Susmita", "Sohana", "Sabana"];
+
  app.get('/', (req, res)=>{
-      res.send('I Know how to open node ...YAY');
+  const fruit ={
+    product : 'ada',
+    price : 220
+  }
+      res.send(fruit);
+ })
+
+ app.get('/fruits/banana', (req, res) =>{
+  res.send({fruit: 'banana', quantity: 1000, price: 10000})
+ })
+
+ app.get('/user/:id', (req, res) =>{
+  const id = req.params.id ;
+  console.log(req.query.sort);
+ const name = users[id]
+ res.send({id, name})
+})
+
+
+ app.get('/user/:id', (req, res) =>{
+     const id = req.params.id ;
+     console.log(req.query.sort);
+    const name = users[id]
+    res.send({id, name})
  })
 
  app.listen(8000, ()=> console.log('listening to port 8000'))
